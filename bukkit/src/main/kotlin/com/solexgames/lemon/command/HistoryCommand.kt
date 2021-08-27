c6e934c41067f73a705a33e9fa9cc4032ee12d49
GrowlyX <rtaofficialtechie@Gmail.com>
2021-08-27 13:32:04 -0400

    Added HistoryCommand.kt with staff history & history command methods

---
+++ bukkit/src/main/kotlin/com/solexgames/lemon/command/HistoryCommand.kt
@@ -0,0 +1,26 @@
+package com.solexgames.lemon.command
+
+import net.evilblock.cubed.acf.BaseCommand
+import net.evilblock.cubed.acf.annotation.CommandAlias
+import net.evilblock.cubed.acf.annotation.Syntax
+import org.bukkit.entity.Player
+
+/**
+ * @author GrowlyX
+ * @since 8/27/2021
+ */
+class HistoryCommand: BaseCommand() {
+
+    @Syntax("<player>")
+    @CommandAlias("history|c|hist")
+    fun onHistory(player: Player, target: String) {
+        // TODO: 8/27/2021 load punishments here and open menu
+    }
+
+    @Syntax("<player>")
+    @CommandAlias("staffhistory|staffhist")
+    fun onStaffHistory(player: Player, target: String) {
+        // TODO: 8/27/2021 load punishments here and open menu
+    }
+
+}
