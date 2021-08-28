495ef95ebb29a87d5eb3da59739e82021941e2dd
GrowlyX <rtaofficialtechie@Gmail.com>
2021-08-28 12:46:04 -0400

    Modified grant handling

--- bukkit/src/main/kotlin/com/solexgames/lemon/handler/GrantHandler.kt
+++ bukkit/src/main/kotlin/com/solexgames/lemon/handler/GrantHandler.kt
@@ -5,14 +5,37 @@ import com.mongodb.client.model.ReplaceOptions
 import com.solexgames.lemon.Lemon
 import com.solexgames.lemon.LemonConstants
 import com.solexgames.lemon.player.grant.Grant
+import com.solexgames.lemon.player.punishment.Punishment
+import com.solexgames.lemon.player.punishment.category.PunishmentCategoryIntensity
 import org.bson.Document
 import java.util.*
 import java.util.concurrent.CompletableFuture
+import java.util.concurrent.atomic.AtomicReference
 import java.util.stream.Collectors
 
 object GrantHandler {
 
-    private var grants: MutableMap<UUID, Grant> = mutableMapOf()
+    private fun fetchGrants(key: String, uuid: UUID, test: (Grant) -> Boolean): CompletableFuture<List<Grant>> {
+        return CompletableFuture.supplyAsync {
+            val list = ArrayList<Grant>()
+
+            Lemon.instance.mongoHandler.punishmentCollection.find(Filters.eq(key, uuid.toString())).forEach {
+                val grant = LemonConstants.GSON.fromJson(it.toJson(), Grant::class.java)
+
+                if (grant != null && test.invoke(grant)) list.add(grant)
+            }
+
+            return@supplyAsync list
+        }
+    }
+
+    fun fetchGrantsByExecutor(uuid: UUID): CompletableFuture<List<Grant>> {
+        return fetchGrants("addedBy", uuid) { true }
+    }
+
+    fun fetchGrantsFor(uuid: UUID): CompletableFuture<List<Grant>> {
+        return fetchGrants("target", uuid) { true }
+    }
 
     fun registerGrant(uuid: UUID, grant: Grant) {
         CompletableFuture.runAsync {
@@ -26,51 +49,42 @@ object GrantHandler {
         }
     }
 
-    fun wipeGrant(uuid: UUID) {
-        val grant = findGrantById(uuid)
+    fun wipeGrant(uuid: UUID, remover: UUID?) {
+        fetchExactGrantById(uuid).whenComplete { grant, throwable ->
+            grant.removedReason = "Removed"
+            grant.removedAt = System.currentTimeMillis()
+            grant.removed = true
+            grant.removedBy = remover
 
-        grant.ifPresent {
-            it.removedReason = "Removed"
-            it.removedAt = System.currentTimeMillis()
-            it.removed = true
-            it.save()
+            grant.save()
         }
     }
 
     fun wipeAllGrantsFor(uuid: UUID) {
-        val grants = findGrants(uuid)
-
-        grants.forEach {
-            it.removedReason = "Removed"
-            it.removedAt = System.currentTimeMillis()
-            it.removed = true
-            it.save()
+        fetchGrantsFor(uuid).whenComplete { grants, _ ->
+            grants.forEach {
+                it.removedReason = "Removed (Grant Wipe)"
+                it.removedAt = System.currentTimeMillis()
+                it.removed = true
+                it.save()
+            }
         }
     }
 
-    /**
-     * Find a grant by its id
-     *
-     * @param uuid the unique identifier of the grant
-     * @return the grant if found
-    */
-    private fun findGrantById(uuid: UUID): Optional<Grant> {
-        return Optional.ofNullable(grants.getOrDefault(uuid, null))
-    }
+    fun fetchExactGrantById(uuid: UUID): CompletableFuture<Grant> {
+        return CompletableFuture.supplyAsync {
+            val reference = AtomicReference<Grant>()
+            val document = Lemon.instance.mongoHandler.grantCollection
+                .find(Filters.eq("uuid", uuid.toString())).first()
 
-    /**
-     * Find all of a player's grants
-     *
-     * @param uuid the unique identifier of the user to find it by
-     * @return the collection of the grants
-     */
-    fun findGrants(uuid: UUID): List<Grant> {
-        return grants.values.stream()
-            .filter { it.target == uuid }
-            .sorted(Comparator.comparingInt {
-                -it.getRank().weight
-            })
-            .collect(Collectors.toList())
+            if (document != null) {
+                val grant = LemonConstants.GSON.fromJson(document.toJson(), Grant::class.java)
+
+                if (grant != null) reference.set(grant)
+            }
+
+            return@supplyAsync reference.get()
+        }
     }
 
 }
