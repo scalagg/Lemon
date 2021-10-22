ab1f64d4895cdee1c98bbe25f1f48e017d68c398
GrowlyX <rtaofficialtechie@Gmail.com>
2021-10-21 22:30:18 -0400

    Fixed target history menu
    Signed-off-by: GrowlyX <rtaofficialtechie@Gmail.com>

--- bukkit/src/main/kotlin/gg/scala/lemon/menu/punishment/PunishmentViewMenu.kt
+++ bukkit/src/main/kotlin/gg/scala/lemon/menu/punishment/PunishmentViewMenu.kt
@@ -88,9 +88,13 @@ class PunishmentViewMenu(
                         fetchPunishments(it).whenComplete { list, _ ->
                             if (list.isEmpty())
                             {
-                                player.sendMessage("${CC.YELLOW}$name${CC.RED} has no recorded ${CC.YELLOW}${it.fancyVersion.lowercase(
-                                    Locale.getDefault()
-                                )}s${CC.RED}.")
+                                player.sendMessage(
+                                    "${CC.YELLOW}$name${CC.RED} has no recorded ${CC.YELLOW}${
+                                        it.fancyVersion.lowercase(
+                                            Locale.getDefault()
+                                        )
+                                    }s${CC.RED}."
+                                )
                                 return@whenComplete
                             }
 
@@ -103,51 +107,54 @@ class PunishmentViewMenu(
                 index += 2
             }
 
-            var newIndex = 20
-
-            val persistentHasPermission = PunishmentCategory.PERSISTENT.filter {
-                player.hasPermission("lemon.punishment.remove.${it.name.lowercase()}")
-            }
-
-            for (it in persistentHasPermission)
+            if (viewType == HistoryViewType.STAFF_HIST)
             {
-                val totalAmount = removed.filter { punishment ->
-                    punishment.category == it
-                }.size
+                var newIndex = 20
 
-                buttons[newIndex] = ItemBuilder(XMaterial.PAPER)
-                    .name(
-                        "${CC.RED}${
-                            when (viewType)
-                            {
-                                HistoryViewType.STAFF_HIST -> "Removed "
-                                HistoryViewType.TARGET_HIST -> ""
-                            }
-                        }${it.fancyVersion + "s"}"
-                    )
-                    .amount(totalAmount)
-                    .addToLore(
-                        "${CC.GRAY}Viewing statistics for the",
-                        "${CC.GRAY}removed ${it.fancyVersion}s category:",
-                        "",
-                        " ${CC.GRAY}Total: ${CC.WHITE}${totalAmount}",
-                        "",
-                        "${CC.YELLOW}Click to view more info."
-                    ).toButton { _, _ ->
-                        fetchPunishmentsRemoved(it).whenComplete { list, _ ->
-                            if (list.isEmpty())
-                            {
-                                player.sendMessage("${CC.YELLOW}$name${CC.RED} has not removed any ${CC.YELLOW}${it.fancyVersion.lowercase()}s${CC.RED}.")
-                                return@whenComplete
-                            }
+                val persistentHasPermission = PunishmentCategory.PERSISTENT.filter {
+                    player.hasPermission("lemon.punishment.remove.${it.name.lowercase()}")
+                }
 
-                            PunishmentDetailedViewMenu(
-                                uuid, it, viewType, list
-                            ).openMenu(player)
+                for (it in persistentHasPermission)
+                {
+                    val totalAmount = removed.filter { punishment ->
+                        punishment.category == it
+                    }.size
+
+                    buttons[newIndex] = ItemBuilder(XMaterial.PAPER)
+                        .name(
+                            "${CC.RED}${
+                                when (viewType)
+                                {
+                                    HistoryViewType.STAFF_HIST -> "Removed "
+                                    HistoryViewType.TARGET_HIST -> ""
+                                }
+                            }${it.fancyVersion + "s"}"
+                        )
+                        .amount(totalAmount)
+                        .addToLore(
+                            "${CC.GRAY}Viewing statistics for the",
+                            "${CC.GRAY}removed ${it.fancyVersion}s category:",
+                            "",
+                            " ${CC.GRAY}Total: ${CC.WHITE}${totalAmount}",
+                            "",
+                            "${CC.YELLOW}Click to view more info."
+                        ).toButton { _, _ ->
+                            fetchPunishmentsRemoved(it).whenComplete { list, _ ->
+                                if (list.isEmpty())
+                                {
+                                    player.sendMessage("${CC.YELLOW}$name${CC.RED} has not removed any ${CC.YELLOW}${it.fancyVersion.lowercase()}s${CC.RED}.")
+                                    return@whenComplete
+                                }
+
+                                PunishmentDetailedViewMenu(
+                                    uuid, it, viewType, list
+                                ).openMenu(player)
+                            }
                         }
-                    }
 
-                newIndex += 2
+                    newIndex += 2
+                }
             }
         }
     }
