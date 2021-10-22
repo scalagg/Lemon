06bb3df99957619eda58284902fafadfacf3af7b
GrowlyX <rtaofficialtechie@Gmail.com>
2021-10-21 22:18:40 -0400

    Updated link regex
    Signed-off-by: GrowlyX <rtaofficialtechie@Gmail.com>

--- bukkit/src/main/kotlin/gg/scala/lemon/handler/FilterHandler.kt
+++ bukkit/src/main/kotlin/gg/scala/lemon/handler/FilterHandler.kt
@@ -16,7 +16,7 @@ object FilterHandler {
     private val regexList = mutableListOf<Regex>()
 
     private val linkRegexList = mutableListOf(
-        "^(http:\\/\\/www\\.|https:\\/\\/www\\.|http:\\/\\/|https:\\/\\/)?[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?\$".toRegex()
+        "^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]".toRegex()
     )
 
     init {
