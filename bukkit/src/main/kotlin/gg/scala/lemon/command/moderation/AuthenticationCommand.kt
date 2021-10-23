ada8ed5673b25fbc653a9c7b4ed22c4497f86eac
GrowlyX <rtaofficialtechie@Gmail.com>
2021-10-22 20:34:19 -0400

    Finally wrote 2faexempt command
    Signed-off-by: GrowlyX <rtaofficialtechie@Gmail.com>

--- bukkit/src/main/kotlin/gg/scala/lemon/command/moderation/AuthenticationCommand.kt
+++ bukkit/src/main/kotlin/gg/scala/lemon/command/moderation/AuthenticationCommand.kt
@@ -3,11 +3,11 @@ package gg.scala.lemon.command.moderation
 import gg.scala.lemon.Lemon
 import gg.scala.lemon.LemonConstants.AUTH_PREFIX
 import gg.scala.lemon.handler.PlayerHandler
+import gg.scala.lemon.player.LemonPlayer
 import gg.scala.lemon.player.metadata.Metadata
 import net.evilblock.cubed.acf.BaseCommand
 import net.evilblock.cubed.acf.ConditionFailedException
 import net.evilblock.cubed.acf.annotation.*
-import net.evilblock.cubed.acf.bukkit.contexts.OnlinePlayer
 import net.evilblock.cubed.util.CC
 import net.evilblock.cubed.util.totp.TimeBasedOneTimePasswordUtil
 import org.bukkit.command.CommandSender
@@ -18,30 +18,36 @@ import org.bukkit.metadata.FixedMetadataValue
  * @author GrowlyX
  * @since 9/27/2021
  */
-class AuthenticationCommand : BaseCommand() {
+class AuthenticationCommand : BaseCommand()
+{
 
     @Syntax("[code]")
     @CommandPermission("lemon.2fa.forced")
     @CommandAlias("auth|2fa|authenticate")
-    fun onAuth(player: Player, code: String) {
+    fun onAuth(player: Player, code: String)
+    {
         val lemonPlayer = PlayerHandler.findPlayer(player).orElse(null)
 
-        if (!lemonPlayer.hasSetupAuthentication()) {
+        if (!lemonPlayer.hasSetupAuthentication())
+        {
             throw ConditionFailedException("You have not setup authentication yet.")
         }
 
-        if (lemonPlayer.isAuthExempt()) {
+        if (lemonPlayer.isAuthExempt())
+        {
             throw ConditionFailedException("You're exempt from authentication.")
         }
 
-        if (lemonPlayer.hasAuthenticatedThisSession()) {
+        if (lemonPlayer.hasAuthenticatedThisSession())
+        {
             throw ConditionFailedException("You do not need to authenticate.")
         }
 
         val correctCode = TimeBasedOneTimePasswordUtil
             .generateCurrentNumberString(lemonPlayer.getAuthSecret())
 
-        if (correctCode != code) {
+        if (correctCode != code)
+        {
             throw ConditionFailedException("The code you've provided is invalid.")
         }
 
@@ -56,8 +62,31 @@ class AuthenticationCommand : BaseCommand() {
         lemonPlayer.authenticateInternal()
 
         player.sendMessage("${AUTH_PREFIX}${CC.GREEN}You've been authenticated.")
+    }
 
-//        BatUtil.makePlayerUnSitOnBat(player)
+    @Syntax("<target>")
+    @CommandAlias("2faexempt")
+    @CommandPermission("op")
+    @Description("Exempt a user from 2fa.")
+    fun onExempt2fa(sender: CommandSender, target: LemonPlayer)
+    {
+        if (target doesNotHave "auth-exempt")
+        {
+            target.updateOrAddMetadata(
+                "auth-exempt",
+                Metadata(true)
+            )
+
+            target.authenticateInternal()
+
+            sender.sendMessage("${AUTH_PREFIX}${CC.GREEN}They are now exempt.")
+        } else
+        {
+            target remove "auth-exempt"
+            target.authenticateInternalReversed()
+
+            sender.sendMessage("${AUTH_PREFIX}${CC.GREEN}They are no longer exempt.")
+        }
     }
 
     @Syntax("<target>")
@@ -65,11 +94,11 @@ class AuthenticationCommand : BaseCommand() {
     @CommandCompletion("@all-players")
     @CommandPermission("lemon.command.remove2fa")
     @Description("Remove & reset a specified player's 2fa.")
-    fun onRemove2fa(sender: CommandSender, target: OnlinePlayer) {
-        val lemonPlayer = PlayerHandler.findPlayer(target.player).orElse(null)
-
-        if (!lemonPlayer.hasSetupAuthentication()) {
-            throw ConditionFailedException("${CC.YELLOW}${target.player.name}${CC.RED} has not setup 2fa.")
+    fun onRemove2fa(sender: CommandSender, lemonPlayer: LemonPlayer)
+    {
+        if (!lemonPlayer.hasSetupAuthentication())
+        {
+            throw ConditionFailedException("${CC.YELLOW}${lemonPlayer.name}${CC.RED} has not setup 2fa.")
         }
 
         lemonPlayer remove "auth-secret"
@@ -77,20 +106,23 @@ class AuthenticationCommand : BaseCommand() {
 
         lemonPlayer.authenticateInternalReversed()
 
-        sender.sendMessage("${CC.SEC}You've reset ${CC.PRI}${target.player.name}'s${CC.SEC} 2fa.")
+        sender.sendMessage("${CC.SEC}You've reset ${CC.PRI}${lemonPlayer.name}'s${CC.SEC} 2fa.")
     }
 
     @CommandAlias("setup2fa")
     @CommandPermission("lemon.2fa.forced")
     @Description("Setup two-factor-authentication for your account.")
-    fun onSetup2fa(player: Player) {
+    fun onSetup2fa(player: Player)
+    {
         val lemonPlayer = PlayerHandler.findPlayer(player).orElse(null)
 
-        if (lemonPlayer.isAuthExempt()) {
+        if (lemonPlayer.isAuthExempt())
+        {
             throw ConditionFailedException("You're exempt from authentication.")
         }
 
-        if (lemonPlayer.hasSetupAuthentication()) {
+        if (lemonPlayer.hasSetupAuthentication())
+        {
             throw ConditionFailedException("You've already setup authentication, if you're looking to reset it, contact a manager.")
         }
 
@@ -103,9 +135,11 @@ class AuthenticationCommand : BaseCommand() {
 
         lemonPlayer.handleAuthMap(base32Secret)
 
-        player.sendMessage(arrayOf(
-            "${AUTH_PREFIX}A map with a QR code has been placed in your inventory.",
-            "${AUTH_PREFIX}${CC.GRAY}Scan the QR code on any applicable authentication app and use the code to authenticate yourself using ${CC.WHITE}/auth <code>${CC.GRAY}."
-        ))
+        player.sendMessage(
+            arrayOf(
+                "${AUTH_PREFIX}A map with a QR code has been placed in your inventory.",
+                "${AUTH_PREFIX}${CC.GRAY}Scan the QR code on any applicable authentication app and use the code to authenticate yourself using ${CC.WHITE}/auth <code>${CC.GRAY}."
+            )
+        )
     }
 }
