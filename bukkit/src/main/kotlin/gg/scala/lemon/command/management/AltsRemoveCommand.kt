2f5591e3013c03218b8a75b67bf012b77d7d9c11
GrowlyX <rtaofficialtechie@Gmail.com>
2021-10-30 17:50:19 -0400

    Added AltsRemoveCommand.kt
    Signed-off-by: GrowlyX <rtaofficialtechie@Gmail.com>

---
+++ bukkit/src/main/kotlin/gg/scala/lemon/command/management/AltsRemoveCommand.kt
@@ -0,0 +1,41 @@
+package gg.scala.lemon.command.management
+
+import gg.scala.lemon.handler.DataStoreHandler
+import gg.scala.lemon.handler.PlayerHandler
+import gg.scala.lemon.player.LemonPlayer
+import net.evilblock.cubed.acf.BaseCommand
+import net.evilblock.cubed.acf.annotation.CommandAlias
+import net.evilblock.cubed.acf.annotation.CommandCompletion
+import net.evilblock.cubed.acf.annotation.CommandPermission
+import net.evilblock.cubed.util.CC
+import org.bukkit.command.CommandSender
+import java.util.*
+
+/**
+ * @author GrowlyX
+ * @since 10/30/2021
+ */
+class AltsRemoveCommand : BaseCommand()
+{
+    @CommandAlias("altsremove")
+    @CommandPermission("lemon.command.altsremove")
+    @CommandCompletion("@all-players")
+    fun onAltsRemove(sender: CommandSender, target: UUID)
+    {
+        sender.sendMessage("${CC.GRAY}Fetching alternate accounts...")
+
+        PlayerHandler.fetchAlternateAccountsFor(target).thenAccept { accounts ->
+            if (accounts.isEmpty())
+            {
+                sender.sendMessage("${CC.RED}No alternate accounts were found.")
+                return@thenAccept
+            }
+
+            accounts.forEach {
+                DataStoreHandler.lemonPlayerLayer.deleteEntry(it.uniqueId.toString())
+            }
+
+            sender.sendMessage("${CC.GREEN}Successfully removed ${CC.D_AQUA}${accounts.size}${CC.GREEN} alternate accounts from the database.")
+        }
+    }
+}
