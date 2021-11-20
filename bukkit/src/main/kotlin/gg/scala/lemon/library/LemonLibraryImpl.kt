9b85b0bf034ed6ef5c8dddc150bbf10603817c3a
GrowlyX <rtaofficialtechie@Gmail.com>
2021-11-19 19:21:50 -0500

    Migrate to kotlin-1.6.0 & update cubed library usage
    Signed-off-by: GrowlyX <rtaofficialtechie@Gmail.com>

--- bukkit/src/main/kotlin/gg/scala/lemon/library/LemonLibraryImpl.kt
+++ bukkit/src/main/kotlin/gg/scala/lemon/library/LemonLibraryImpl.kt
@@ -6,16 +6,10 @@ import gg.scala.commons.library.model.Library
  * @author GrowlyX
  * @since 10/14/2021
  */
+@Library(
+    groupId = "com.github.mkotb",
+    artifactId = "ConfigAPI",
+    version = "ac23157173",
+    repository = "https://jitpack.io/"
+)
 object LemonLibraryImpl
-{
-
-    @JvmStatic
-    val CONFIG_API = Library(
-        groupId = "com.github.mkotb",
-        artifactId = "ConfigAPI",
-        version = "ac23157173",
-    ).repository(
-        "https://jitpack.io"
-    )
-
-}
