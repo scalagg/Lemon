b4142a5fe27346439805911c063c785b5b241178
GrowlyX <growlyxdev@gmail.com>
2022-07-14 18:08:24 -0400

    Fix nullability issue with global alert permissions

--- bukkit/src/main/kotlin/gg/scala/lemon/handler/RedisHandler.kt
+++ bukkit/src/main/kotlin/gg/scala/lemon/handler/RedisHandler.kt
@@ -108,13 +108,13 @@ object RedisHandler
         val permission = message
             .retrieveNullable<String>("permission")
 
-        if (permission!!.isNotBlank())
+        if (!permission.isNullOrBlank())
         {
             broadcast(newMessage, permission)
-        } else
-        {
-            Bukkit.broadcastMessage(newMessage)
+            return
         }
+
+        Bukkit.broadcastMessage(newMessage)
     }
 
     @Subscribe("mass-whitelist")
