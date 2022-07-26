8dd0c5141bfc7f0335cb6895d06c8b3987bdd76b
GrowlyX <growlyxdev@gmail.com>
2022-07-26 19:41:54 -0400

    Prefixed nametags

--- bukkit/src/main/kotlin/gg/scala/lemon/player/nametag/DefaultNametagProvider.kt
+++ bukkit/src/main/kotlin/gg/scala/lemon/player/nametag/DefaultNametagProvider.kt
@@ -1,15 +1,19 @@
 package gg.scala.lemon.player.nametag
 
+import gg.scala.lemon.minequest
 import gg.scala.lemon.util.QuickAccess.realRank
 import net.evilblock.cubed.nametag.NametagInfo
 import net.evilblock.cubed.nametag.NametagProvider
 import org.bukkit.entity.Player
 
-object DefaultNametagProvider: NametagProvider("default", 10) {
-
-    override fun fetchNametag(toRefresh: Player, refreshFor: Player): NametagInfo {
+object DefaultNametagProvider : NametagProvider("default", 10)
+{
+    override fun fetchNametag(toRefresh: Player, refreshFor: Player): NametagInfo
+    {
         return createNametag(
-            realRank(toRefresh).color, ""
+            realRank(toRefresh).let {
+                if (minequest()) "${it.prefix} ${it.color}" else it.color
+            }, ""
         )
     }
 }
