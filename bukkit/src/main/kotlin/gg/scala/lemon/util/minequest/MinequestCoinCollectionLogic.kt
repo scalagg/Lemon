936755ea2bbfcd4ea9f41a179db0737e0272fa0b
GrowlyX <growlyxdev@gmail.com>
2022-08-04 18:00:54 -0400

    Handle

---
+++ bukkit/src/main/kotlin/gg/scala/lemon/util/minequest/MinequestCoinCollectionLogic.kt
@@ -0,0 +1,12 @@
+package gg.scala.lemon.util.minequest
+
+import org.bukkit.entity.Player
+
+/**
+ * @author GrowlyX
+ * @since 8/4/2022
+ */
+object MinequestCoinCollectionLogic
+{
+    var onCollection = { player: Player -> }
+}
--- bukkit/src/main/kotlin/gg/scala/lemon/util/minequest/commands/MinequestCommands.kt
+++ bukkit/src/main/kotlin/gg/scala/lemon/util/minequest/commands/MinequestCommands.kt
@@ -5,13 +5,19 @@ import gg.scala.commons.acf.annotation.CommandPermission
 import gg.scala.commons.command.ScalaCommand
 import gg.scala.lemon.util.QuickAccess.username
 import gg.scala.lemon.util.minequest.MinequestChatImages
+import gg.scala.lemon.util.minequest.MinequestCoinCollectionLogic
 import gg.scala.lemon.util.minequest.MinequestIcon
 import gg.scala.lemon.util.minequest.platinum.menu.PlatinumColorChangeMenu
+import me.lucko.helper.Events
+import net.evilblock.cubed.util.CC
 import org.bukkit.Bukkit
 import org.bukkit.ChatColor
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
+import org.bukkit.event.EventPriority
+import org.bukkit.event.player.AsyncPlayerChatEvent
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 /**
  * @author GrowlyX
@@ -42,16 +48,31 @@ object MinequestCommands : ScalaCommand()
         {
             when (index + 1)
             {
-                1 -> Bukkit.broadcastMessage(" 䰘" + str + "     " + ChatColor.BOLD + target.username() + ChatColor.AQUA + " supported the server by")
+                1 -> Bukkit.broadcastMessage(" 䰘" + str + "     " + CC.B_AQUA + target.username() + ChatColor.AQUA + " supported the server by")
                 2 -> Bukkit.broadcastMessage(" 䰘" + str + "     " + ChatColor.AQUA + "purchasing an item from the store!")
                 3 -> Bukkit.broadcastMessage(" 䰘$str")
                 4 -> Bukkit.broadcastMessage(" 䰘$str")
-                5 -> Bukkit.broadcastMessage(" 䰘" + str + "     " + ChatColor.GRAY + "Say " + ChatColor.WHITE + ChatColor.BOLD.toString() + "'GG'" + ChatColor.GRAY + " in chat for " + ChatColor.GOLD + "10 " + ChatColor.WHITE + MinequestIcon.COIN.character)
-                6 -> Bukkit.broadcastMessage(" 䰘" + str + "     " + ChatColor.AQUA + "store.minequest.gg")
+                5 -> Bukkit.broadcastMessage(" 䰘" + str + "     " + ChatColor.GRAY + "Say " + ChatColor.WHITE + ChatColor.BOLD.toString() + "GG" + ChatColor.GRAY + " in chat for " + ChatColor.GOLD + "10 " + ChatColor.WHITE + MinequestIcon.COIN.character + CC.GRAY + "!")
+                6 -> Bukkit.broadcastMessage(" 䰘" + str + "     " + ChatColor.DARK_AQUA + "store.minequest.gg")
             }
         }
 
         Bukkit.broadcastMessage("")
-        // TODO: handle gg coin logic
+
+        val coinCollection = mutableListOf<UUID>()
+
+        Events
+            .subscribe(
+                AsyncPlayerChatEvent::class.java,
+                EventPriority.HIGHEST
+            )
+            .expireAfter(10L, TimeUnit.SECONDS)
+            .filter {
+                !coinCollection.contains(it.player.uniqueId) &&
+                it.message.lowercase().startsWith("gg")
+            }
+            .handler {
+                MinequestCoinCollectionLogic.onCollection(it.player)
+            }
     }
 }
