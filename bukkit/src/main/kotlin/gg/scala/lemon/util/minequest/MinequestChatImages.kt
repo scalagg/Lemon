ab50e2e1ccce28157bb68e7444ace13e5bcc6f32
GrowlyX <growlyxdev@gmail.com>
2022-08-17 16:50:23 -0400

    Add minequest image mappings for 1.13+

--- bukkit/src/main/kotlin/gg/scala/lemon/util/minequest/MinequestChatImages.kt
+++ bukkit/src/main/kotlin/gg/scala/lemon/util/minequest/MinequestChatImages.kt
@@ -1,44 +1,131 @@
 package gg.scala.lemon.util.minequest
 
+import net.evilblock.cubed.util.nms.MinecraftProtocol
+import org.bukkit.entity.Player
+
 /**
  * @author GrowlyX
  * @since 7/25/2022
  */
 object MinequestChatImages
 {
-    fun testImage() = listOf(
-        "伀㼆企㼆伂㼆伃㼆伄㼆伅㼆伆㼆伇㼆伈",
-        "伐㼆休㼆伒㼆伓㼆伔㼆伕㼆伖㼆众㼆优",
-        "传㼆伡㼆伢㼆伣㼆伤㼆伥㼆伦㼆伧㼆伨",
-        "估㼆伱㼆伲㼆伳㼆伴㼆伵㼆伶㼆伷㼆伸",
-        "佀㼆佁㼆佂㼆佃㼆佄㼆佅㼆但㼆佇㼆佈",
-        "佐㼆佑㼆佒㼆体㼆佔㼆何㼆佖㼆佗㼆佘",
-        "你㼆佡㼆佢㼆佣㼆佤㼆佥㼆佦㼆佧㼆佨",
-        "佰㼆佱㼆佲㼆佳㼆佴㼆併㼆佶㼆佷㼆佸"
-    )
+    fun triviaImage(player: Player): List<String>
+    {
+        val protocol = MinecraftProtocol
+            .getPlayerVersion(player)
 
-    fun purchaseImage() = listOf(
-        "㼆谀㼆谁㼆谂㼆调㼆谄㼆谅",
-        "㼆谐㼆谑㼆谒㼆谓㼆谔㼆谕",
-        "㼆谠㼆谡㼆谢㼆谣㼆谤㼆谥",
-        "㼆谰㼆谱㼆谲㼆谳㼆谴㼆谵",
-        "㼆豀㼆豁㼆豂㼆豃㼆豄㼆豅",
-        "㼆豐㼆豑㼆豒㼆豓㼆豔㼆豕"
-    )
+        return if (protocol <= 393)
+        {
+            listOf(
+                "" // TODO
+            )
+        } else
+        {
+            listOf("ŉ")
+        }
+    }
 
-    fun questStartedImage() = listOf(
-        "䲰㼆䲱㼆䲲㼆䲳㼆䲴㼆䲵㼆䲶㼆䲷㼆䲸㼆䲹㼆䲺㼆䲺㼆䲺㼆䲺㼆䲺㼆䲺㼆䱠㼆䱡㼆䱢㼆䱣㼆䱤㼆䱥㼆䱦㼆䱧㼆䱨㼆䱩㼆䱪㼆䱫㼆䱬㼆䱭",
-        "䳀㼆䳁㼆䳂㼆䳃㼆䳄㼆䳅㼆䳆㼆䳇㼆䳈㼆䳉㼆䳊㼆䳋㼆䳌㼆䳍㼆䳎㼆䳏㼆䱰㼆䱱㼆䱲㼆䱳㼆䱴㼆䱵㼆䱶㼆䱷㼆䱸㼆䱹㼆䱺㼆䱻㼆䱼㼆䱽",
-        "䳐㼆䳑㼆䳒㼆䳓㼆䳔㼆䳕㼆䳖㼆䳗㼆䳘㼆䳙㼆䳚㼆䳛㼆䳜㼆䳝㼆䳞㼆䳟㼆䲀㼆䲁㼆䲂㼆䲃㼆䲄㼆䲅㼆䲆㼆䲇㼆䲈㼆䲉㼆䲊㼆䲋㼆䲌㼆䲍",
-        "䳠㼆䳡㼆䳢㼆䳣㼆䳤㼆䳥㼆䳦㼆䳧㼆䳨㼆䳩㼆䳪㼆䳫㼆䳬㼆䳭㼆䳮㼆䳯㼆䲐㼆䲑㼆䲒㼆䲓㼆䲔㼆䲕㼆䲖㼆䲗㼆䲘㼆䲙㼆䲚㼆䲛㼆䲜㼆䲝",
-        "䳰㼆䳱㼆䳲㼆䳳㼆䳴㼆䳵㼆䳶㼆䳷㼆䳸㼆䳹㼆䳺㼆䳻㼆䳼㼆䳽㼆䳾㼆䳿㼆䲠㼆䲡㼆䲢㼆䲣㼆䲤㼆䲥㼆䲦㼆䲧㼆䲨㼆䲩㼆䲪㼆䲫㼆䲬㼆䲭"
-    )
+    fun eventImage(player: Player): List<String>
+    {
+        val protocol = MinecraftProtocol
+            .getPlayerVersion(player)
 
-    fun questCompleteImage() = listOf(
-        "趰㼆趱㼆趲㼆足㼆趴㼆趵㼆趶㼆趷㼆趸㼆趹㼆趺㼆趻㼆趼㼆趽㼆趾㼆趿㼆赠㼆赡㼆赢㼆赣㼆赤㼆赥㼆赦㼆赧㼆赨㼆赩㼆赪㼆赫㼆赬㼆赭",
-        "跀㼆跁㼆跂㼆跃㼆跄㼆跅㼆跆㼆跇㼆跈㼆跉㼆跊㼆跋㼆跌㼆跍㼆跎㼆跏㼆走㼆赱㼆赲㼆赳㼆赴㼆赵㼆赶㼆起㼆赸㼆赹㼆赺㼆赻㼆赼㼆赽",
-        "跐㼆跑㼆跒㼆跓㼆跔㼆跕㼆跖㼆跗㼆跘㼆跙㼆跚㼆跛㼆跜㼆距㼆跞㼆跟㼆趀㼆趁㼆趂㼆趃㼆趄㼆超㼆趆㼆趇㼆趈㼆趉㼆越㼆趋㼆趌㼆趍",
-        "跠㼆跡㼆跢㼆跣㼆跤㼆跥㼆跦㼆跧㼆跨㼆跩㼆跪㼆跫㼆跬㼆跭㼆跮㼆路㼆趐㼆趑㼆趒㼆趓㼆趔㼆趕㼆趖㼆趗㼆趘㼆趙㼆趚㼆趛㼆趜㼆趝",
-        "跰㼆跱㼆跲㼆跳㼆跴㼆践㼆跶㼆跷㼆跸㼆跹㼆跺㼆跻㼆跼㼆跽㼆跾㼆跿㼆趠㼆趡㼆趢㼆趣㼆趤㼆趥㼆趦㼆趧㼆趨㼆趩㼆趪㼆趫㼆趬㼆趭"
-    )
+        return if (protocol <= 393)
+        {
+            listOf(
+                "" // TODO
+            )
+        } else
+        {
+            listOf("ŋ")
+        }
+    }
+
+    fun levelUpBlueImage(player: Player): List<String>
+    {
+        val protocol = MinecraftProtocol
+            .getPlayerVersion(player)
+
+        return if (protocol <= 393)
+        {
+            listOf(
+                "" // TODO
+            )
+        } else
+        {
+            listOf("ł")
+        }
+    }
+
+    fun levelUpGrayImage(player: Player): List<String>
+    {
+        val protocol = MinecraftProtocol
+            .getPlayerVersion(player)
+
+        return if (protocol <= 393)
+        {
+            listOf(
+                "" // TODO
+            )
+        } else
+        {
+            listOf("Ł")
+        }
+    }
+
+    fun thankYouImage(player: Player): List<String>
+    {
+        val protocol = MinecraftProtocol
+            .getPlayerVersion(player)
+
+        return if (protocol <= 393)
+        {
+            listOf(
+                "" // TODO
+            )
+        } else
+        {
+            listOf("Ŋ")
+        }
+    }
+
+    fun questStartedImage(player: Player): List<String>
+    {
+        val protocol = MinecraftProtocol
+            .getPlayerVersion(player)
+
+        return if (protocol <= 393)
+        {
+            listOf(
+                "䲰㼆䲱㼆䲲㼆䲳㼆䲴㼆䲵㼆䲶㼆䲷㼆䲸㼆䲹㼆䲺㼆䲺㼆䲺㼆䲺㼆䲺㼆䲺㼆䱠㼆䱡㼆䱢㼆䱣㼆䱤㼆䱥㼆䱦㼆䱧㼆䱨㼆䱩㼆䱪㼆䱫㼆䱬㼆䱭",
+                "䳀㼆䳁㼆䳂㼆䳃㼆䳄㼆䳅㼆䳆㼆䳇㼆䳈㼆䳉㼆䳊㼆䳋㼆䳌㼆䳍㼆䳎㼆䳏㼆䱰㼆䱱㼆䱲㼆䱳㼆䱴㼆䱵㼆䱶㼆䱷㼆䱸㼆䱹㼆䱺㼆䱻㼆䱼㼆䱽",
+                "䳐㼆䳑㼆䳒㼆䳓㼆䳔㼆䳕㼆䳖㼆䳗㼆䳘㼆䳙㼆䳚㼆䳛㼆䳜㼆䳝㼆䳞㼆䳟㼆䲀㼆䲁㼆䲂㼆䲃㼆䲄㼆䲅㼆䲆㼆䲇㼆䲈㼆䲉㼆䲊㼆䲋㼆䲌㼆䲍",
+                "䳠㼆䳡㼆䳢㼆䳣㼆䳤㼆䳥㼆䳦㼆䳧㼆䳨㼆䳩㼆䳪㼆䳫㼆䳬㼆䳭㼆䳮㼆䳯㼆䲐㼆䲑㼆䲒㼆䲓㼆䲔㼆䲕㼆䲖㼆䲗㼆䲘㼆䲙㼆䲚㼆䲛㼆䲜㼆䲝",
+                "䳰㼆䳱㼆䳲㼆䳳㼆䳴㼆䳵㼆䳶㼆䳷㼆䳸㼆䳹㼆䳺㼆䳻㼆䳼㼆䳽㼆䳾㼆䳿㼆䲠㼆䲡㼆䲢㼆䲣㼆䲤㼆䲥㼆䲦㼆䲧㼆䲨㼆䲩㼆䲪㼆䲫㼆䲬㼆䲭"
+            )
+        } else
+        {
+            listOf("Š")
+        }
+    }
+
+    fun questCompleteImage(player: Player): List<String>
+    {
+        val protocol = MinecraftProtocol
+            .getPlayerVersion(player)
+
+        return if (protocol <= 393)
+        {
+            listOf(
+                "趰㼆趱㼆趲㼆足㼆趴㼆趵㼆趶㼆趷㼆趸㼆趹㼆趺㼆趻㼆趼㼆趽㼆趾㼆趿㼆赠㼆赡㼆赢㼆赣㼆赤㼆赥㼆赦㼆赧㼆赨㼆赩㼆赪㼆赫㼆赬㼆赭",
+                "跀㼆跁㼆跂㼆跃㼆跄㼆跅㼆跆㼆跇㼆跈㼆跉㼆跊㼆跋㼆跌㼆跍㼆跎㼆跏㼆走㼆赱㼆赲㼆赳㼆赴㼆赵㼆赶㼆起㼆赸㼆赹㼆赺㼆赻㼆赼㼆赽",
+                "跐㼆跑㼆跒㼆跓㼆跔㼆跕㼆跖㼆跗㼆跘㼆跙㼆跚㼆跛㼆跜㼆距㼆跞㼆跟㼆趀㼆趁㼆趂㼆趃㼆趄㼆超㼆趆㼆趇㼆趈㼆趉㼆越㼆趋㼆趌㼆趍",
+                "跠㼆跡㼆跢㼆跣㼆跤㼆跥㼆跦㼆跧㼆跨㼆跩㼆跪㼆跫㼆跬㼆跭㼆跮㼆路㼆趐㼆趑㼆趒㼆趓㼆趔㼆趕㼆趖㼆趗㼆趘㼆趙㼆趚㼆趛㼆趜㼆趝",
+                "跰㼆跱㼆跲㼆跳㼆跴㼆践㼆跶㼆跷㼆跸㼆跹㼆跺㼆跻㼆跼㼆跽㼆跾㼆跿㼆趠㼆趡㼆趢㼆趣㼆趤㼆趥㼆趦㼆趧㼆趨㼆趩㼆趪㼆趫㼆趬㼆趭"
+            )
+        } else
+        {
+            listOf("Λ")
+        }
+    }
 }
