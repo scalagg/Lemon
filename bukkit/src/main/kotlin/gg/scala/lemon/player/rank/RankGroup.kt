a3424d143525578215ffff00e85ab8d67935ecc2
GrowlyX <growlyxdev@gmail.com>
2023-02-10 18:03:00 -0500

    feat: add RankGroup template

--- bukkit/src/main/kotlin/gg/scala/lemon/player/rank/RankGroup.kt
+++ bukkit/src/main/kotlin/gg/scala/lemon/player/rank/RankGroup.kt
@@ -1,9 +1,13 @@
 package gg.scala.lemon.player.rank
 
+import gg.scala.lemon.scope.ServerScope
+
 /**
  * @author GrowlyX
  * @since 2/10/2023
  */
 data class RankGroup(
-
+    val id: String,
+    val permissions: List<String>,
+    val scope: ServerScope? = null
 )
