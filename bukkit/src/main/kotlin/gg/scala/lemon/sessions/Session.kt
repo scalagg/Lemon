79fdc3a3f4a286af0eb332ba24f77335536d0cab
Subham K. <me@growly.gg>
2024-08-09 01:41:15 -0400

    chore: mark playerID as indexed

--- bukkit/src/main/kotlin/gg/scala/lemon/sessions/Session.kt
+++ bukkit/src/main/kotlin/gg/scala/lemon/sessions/Session.kt
@@ -3,6 +3,7 @@ package gg.scala.lemon.sessions
 import gg.scala.common.Savable
 import gg.scala.commons.annotations.Model
 import gg.scala.store.controller.DataStoreObjectControllerCache
+import gg.scala.store.controller.annotations.Indexed
 import gg.scala.store.storage.storable.IDataStoreObject
 import gg.scala.store.storage.type.DataStoreStorageType
 import java.util.*
@@ -14,6 +15,7 @@ import java.util.*
 @Model
 data class Session(
     override val identifier: UUID = UUID.randomUUID(),
+    @Indexed
     val playerID: UUID,
     val timestamp: Long,
     var length: Long = 0L
