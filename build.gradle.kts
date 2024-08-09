2aae1b124def9ced31d6e0a3a960093ab2cd09ef
Subham K. <me@growly.gg>
2024-08-09 01:31:05 -0400

    chore: bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ var currentBranch: String = grgit.branch.current().name
 
 allprojects {
     group = "gg.scala.lemon"
-    version = "1.8.1${
+    version = "1.8.2${
         if (currentBranch == "develop") "-dev" else ""
     }"
 
