8e284308c730a743fed69c92a98478f31b3a8ae6
Subham K. <me@growly.gg>
2024-08-21 19:07:23 -0400

    fix: mark as service

--- bukkit/src/main/kotlin/gg/scala/lemon/filter/ml/ChatMLDataSync.kt
+++ bukkit/src/main/kotlin/gg/scala/lemon/filter/ml/ChatMLDataSync.kt
@@ -2,12 +2,14 @@ package gg.scala.lemon.filter.ml
 
 import gg.scala.commons.persist.datasync.DataSyncKeys
 import gg.scala.commons.persist.datasync.DataSyncService
+import gg.scala.flavor.service.Service
 import net.kyori.adventure.key.Key
 
 /**
  * @author GrowlyX
  * @since 8/21/2024
  */
+@Service
 object ChatMLDataSync : DataSyncService<ChatMLConfig>()
 {
     object ChatMLDataSyncKeys : DataSyncKeys
