f6093d67b83b0cce974575574eaf286058f61569
Subham K. <me@growly.gg>
2024-09-29 22:51:57 -0400

    chore: remove old dependency

--- bukkit/build.gradle.kts
+++ bukkit/build.gradle.kts
@@ -5,7 +5,6 @@ dependencies {
     compileOnly("com.github.mkotb:ConfigAPI:e1c8df3f13")
     compileOnly("com.github.MilkBowl:VaultAPI:1.7.1")
 
-    compileOnly("me.clip:placeholderapi:2.11.2")
     compileOnly("com.comphenix.protocol:ProtocolLib:4.8.0")
     compileOnly("me.lucko:spark-api:0.1-SNAPSHOT")
     compileOnly("com.github.LunarClient:BukkitAPI:1.0.1")
